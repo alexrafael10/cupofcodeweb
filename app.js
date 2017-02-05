297dfc054f1143c174077cbc4f21135a8f7a71ac
Alex <alexrafael10@gmail.com>
2017-02-05 12:18:31 -0400

    favicon

--- app.js
+++ app.js
@@ -35,6 +35,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(favicon(__dirname+'/public/images/brand-min.png'));
 
 app.use('/', index);
 
